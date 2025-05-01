ea95dc56318842941f8af4bc818b4482edbed3db
Prashant Raj <pr2125116@gmail.com>
2025-05-01 23:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this._age;
+	}
 
-class Teacher extends Person {}
+	set name(newName){
+		if(newName.length > 0){
+			this._name = newName;
+		}
+		else{
+			console.error("Name cannot be empty");
+		}
+	}
+	
+	set age(newAge){
+		if(age > 0){
+			this._age = newAge;
+		}
+		else{
+			console.error("Age cannot be less than 0");
+		}
+	}
+}
 
+class Student extends Person {
+	study() {
+		console.log(`${this.name()} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name() is teaching}`)
+	}
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
